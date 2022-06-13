f4b44a4d4a0e845cc613db7db71a40c6e5d2eade
andrey-chekunov <79117325352@yandex.ru>
2022-06-13 12:52:53 +0300

    Add directory Zadacha_4 and solved the task

---
+++ Zadacha_4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2 3 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22*/
+
+Console.Write("Input first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input third number: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (number2 > max)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+Console.Write(max + " is greater");
